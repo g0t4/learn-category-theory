ac19e3b0e1b050dc013296dd64c5821936931362
Wes Higbee <wes.mcclure@gmail.com>
2024-08-07 00:14:58 -0600

    function definitions

--- haskell/test.hs
+++ haskell/test.hs
@@ -6,5 +6,17 @@ main = putStrLn "Hello, Haskell!"
 
 
 -- repl: ghci
---   then paste: putStrLn "Hello, Haskell!" 
---   or paste main and invoke it
+--  putStrLn "Hello, Haskell!"
+--  :load test.hs
+--  main
+--  doubleit 2
+--  doubleit (-1) -- put negative integer literals in parens (else IIUC appears indistinguishable from minus 1)
+
+-- haskell has incredible type inference (rarely need to specify types), nonetheless a function (morphism) can specify/constrain types:
+doubleit :: Int -> Int -- limit to integer type
+doubleit x = x + x -- func defintion (name params = body)
+-- doubleit 1 -- succeeds
+-- doubleit 1.0 -- fails
+
+-- see vscode code lens for inferred types
+doubleanyinferred x = x + x
