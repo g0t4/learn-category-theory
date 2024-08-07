49c27db3e3536f7e23328521196ca5ba6822fb40
Wes Higbee <wes.mcclure@gmail.com>
2024-08-07 16:44:53 -0600

    MyMonoid

---
+++ haskell/monoid.hs
@@ -0,0 +1,18 @@
+-- from ctfp page 32 (renamed m_empty instead of mempty)
+class MyMonoid m where
+  m_empty :: m
+  m_append :: m -> m -> m
+
+-- my impl of a MyMonoid for type String (based on some of explaining classes in the haskell cheatsheet)
+instance MyMonoid String where
+  m_empty = ""
+  m_append a b = a ++ b
+
+testMyMonoidString = m_append "foo" "bar"
+
+instance MyMonoid Int where
+  m_empty = 0
+  m_append a b = a + b
+
+testMyMonoidInteger :: Int
+testMyMonoidInteger = m_append 1 2
