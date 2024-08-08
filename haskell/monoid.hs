02f5865b039f2ab7897d9d790d082b6dc8949180
Wes Higbee <wes.mcclure@gmail.com>
2024-08-08 13:48:10 -0500

    experiment with foldl vs sum/concat vs MyMonoid

--- haskell/monoid.hs
+++ haskell/monoid.hs
@@ -19,7 +19,25 @@ testMyMonoidInteger :: Int
 testMyMonoidInteger = m_append 1 2
 
 -- infix operator => two arg function (surround w/ parens)
-concats = (++)
+concats = (++) -- function equality (point-free)
 -- foo = concats "foo" "bar"
+
 combined = (++) "foo" "bar"
+
 combined2 = "foo" ++ "bar"
+
+-- https://www.idris-lang.org/docs/current/prelude_doc/docs/Prelude.Foldable.html
+-- Int type
+sum1 = foldl (+) 0 [1 .. 5] -- use foldl to sum numbers, suggests sum
+
+sum2 = sum [1 .. 5]
+
+sum3 :: Int
+sum3 = foldl m_append m_empty [1 .. 5] -- use MyMonoid
+
+-- string type
+alpha1 = foldl (++) "" ["a", "b", "c"] -- foldl suggests concat
+
+alpha2 = concat ["a", "b", "c"]
+
+alpha3 = foldl m_append m_empty ["a", "b", "c"]
