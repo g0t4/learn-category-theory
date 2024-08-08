890fa1c380add236849a0851d5922c1bc4a105af
Wes Higbee <wes.mcclure@gmail.com>
2024-08-08 14:41:36 -0500

    print = putStrLn . show

--- haskell/UseFoo.hs
+++ haskell/UseFoo.hs
@@ -4,6 +4,13 @@ main :: IO ()
 main = do
   putStrLn ("3 + 4 = " ++ show (Foo.addit 3 4))
   putStrLn $ "3 + 4 = " ++ show (Foo.addit 3 4) -- \$ to reduce parens()
+  putStrLn ""
+
+  print (show (Foo.addit 3 4))
+  print $ show (Foo.addit 3 4)
+  print $ show $ Foo.addit 3 4
+  putStrLn ""
+
   let multiplied = Foo.multit 2 2
   putStrLn $ "2 * 2 = " ++ show multiplied
   putStrLn $ "2 * 2 = " ++ Prelude.show multiplied
