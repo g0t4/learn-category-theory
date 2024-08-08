05a0139201e300e01fc3702a8093594cf26057fc
Wes Higbee <wes.mcclure@gmail.com>
2024-08-08 14:50:02 -0500

    notes

--- haskell/UseFoo.hs
+++ haskell/UseFoo.hs
@@ -14,6 +14,7 @@ main = do
   let multiplied = Foo.multit 2 2
   putStrLn $ "2 * 2 = " ++ show multiplied
   putStrLn $ "2 * 2 = " ++ Prelude.show multiplied
+  putStrLn $ "2 * 2 = " <> show multiplied -- <> more general than ++ IIUC
 
 -- can use {} but using spaces for scope is recommended
 main2 :: IO ()
