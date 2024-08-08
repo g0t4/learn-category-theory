06ade1420397c85f23519e0458d615b1e3f7c1ea
Wes Higbee <wes.mcclure@gmail.com>
2024-08-07 21:21:54 -0600

    (++)

--- haskell/monoid.hs
+++ haskell/monoid.hs
@@ -6,13 +6,17 @@ class MyMonoid m where
 -- my impl of a MyMonoid for type String (based on some of explaining classes in the haskell cheatsheet)
 instance MyMonoid String where
   m_empty = ""
-  m_append a b = a ++ b
+
+  m_append = (++) -- m_append a b = a ++ b
 
 testMyMonoidString = m_append "foo" "bar"
 
 instance MyMonoid Int where
   m_empty = 0
-  m_append a b = a + b
+  m_append = (+) -- m_append a b = a + b
 
 testMyMonoidInteger :: Int
 testMyMonoidInteger = m_append 1 2
+
+concats = (++)
+-- foo = concats "foo" "bar"
