e968d4164a7bad388ca434ffd80004c33d52a8dc
Wes Higbee <wes.mcclure@gmail.com>
2024-08-09 12:44:55 -0500

    work on looping on my own

---
+++ haskell/loops.hs
@@ -0,0 +1,14 @@
+remainder = rem
+
+fizzBuzzFor num
+  | num `remainder` 15 == 0 = "fizzbuzz"
+  | num `remainder` 5 == 0 = "buzz"
+  | num `remainder` 3 == 0 = "fizz"
+  | otherwise = show num
+
+main = do
+  let numbers = [1 .. 15]
+  let fors = map fizzBuzzFor numbers
+  print fors
+  let combined = foldl (++) "" fors
+  print combined
