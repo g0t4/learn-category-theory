d9fae723157ae8169aec815a032f038226690444
Wes Higbee <wes.mcclure@gmail.com>
2024-08-10 17:56:24 -0500

    evaluate

--- haskell/lazy.hs
+++ haskell/lazy.hs
@@ -1,7 +1,7 @@
 positiveIntegers =
-  -- cons operator affords a generator like effect with lazy load
+  -- cons operator affords a generator like effect with lazy evaluation
   let rest n = n : rest (n + 1)
    in rest 0 -- start at 0
 
-testLazyLoad = do
+testLazyEvaluate = do
   print $ positiveIntegers !! 4 -- only need to evaluate first 4 elements of list and thus infinite list is not created
