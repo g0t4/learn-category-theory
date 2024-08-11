5a3e76d0f70f3d5325ec000d3de105c32014dc75
Wes Higbee <wes.mcclure@gmail.com>
2024-08-10 19:21:06 -0500

    myFindFirst

--- haskell/lazy.hs
+++ haskell/lazy.hs
@@ -35,3 +35,15 @@ findFirst predicate =
 -- lazy eval + foldr impl:
 -- b/c of how findFirst is defined, it can stop at the first matching element that satisifes the predicate and thus due to lazy eval we stop at that point and can foldr across an infinite list (b/c if we are contingent only on current item and don't express the accumulated value as a combination of subsequent items too then we can stop at the current item that satisifes a given predicate... interesting)...
 -- IF HOWEVER I Used my REVERSE impl of myFoldR then findFirst wouldn't work for an infinite list...
+
+-- THAT SAID GOOD GOD WHY USE foldr to find the first item in a list!
+
+myFindFirst predicate (head : rest)
+  | predicate head = [head]
+  | otherwise = myFindFirst predicate rest
+myFindFirst _ [] = [] -- ok to put this one last though might be best to put it first since it is a base case and it is not that intuitive to know that (head : rest) won't match an empty list
+
+testMyFindFirst = do
+  print $ myFindFirst (> 5) [1, 3 .. 10]
+  print $ myFindFirst (> 9) [1, 3 .. 10]
+  print $ myFindFirst (> 100) positiveIntegers
