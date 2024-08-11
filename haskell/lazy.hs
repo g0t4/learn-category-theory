30483406cceb3b425c59fda46c090f8033720784
Wes Higbee <wes.mcclure@gmail.com>
2024-08-10 19:14:59 -0500

    notes about foldr and why the func provided can lead to lazy eval and early terimination and not need to process an infinite list.. benefit of lazy eval

--- haskell/lazy.hs
+++ haskell/lazy.hs
@@ -19,3 +19,19 @@ myCycle list =
 -- much more succinct!
 myCycle2 [] = []
 myCycle2 list = list <> myCycle2 list
+
+firstFive = filter (< 5) positiveIntegers
+
+testFilter = do
+  print $ firstFive
+
+findFirst predicate =
+  foldr findHelper []
+  where
+    findHelper listElement maybeFound
+      | predicate listElement = [listElement]
+      | otherwise = maybeFound
+
+-- lazy eval + foldr impl:
+-- b/c of how findFirst is defined, it can stop at the first matching element that satisifes the predicate and thus due to lazy eval we stop at that point and can foldr across an infinite list (b/c if we are contingent only on current item and don't express the accumulated value as a combination of subsequent items too then we can stop at the current item that satisifes a given predicate... interesting)...
+-- IF HOWEVER I Used my REVERSE impl of myFoldR then findFirst wouldn't work for an infinite list...
