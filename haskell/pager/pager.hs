4467861e6a4bb188cab0eed03223dae82005a7f8
Wes Higbee <wes.mcclure@gmail.com>
2024-08-15 16:37:27 -0500

    use Sys.Env insteaad

--- haskell/pager/pager.hs
+++ haskell/pager/pager.hs
@@ -1,4 +1,4 @@
-import System.Directory.Internal.Prelude (getArgs)
+import System.Environment (getArgs)
 
 -- based on Effective Haskell book example, with my own improv along the way
 
@@ -9,7 +9,7 @@ myPrintArgs args = foldl (\accum current -> accum >> putStrLn current) noop args
 
 -- runhaskell pager.hs --foo bar
 main =
-  -- getArgs >>= myPrintArgs
   runHCat
 
-runHCat = return ()
+runHCat =
+  getArgs >>= myPrintArgs
