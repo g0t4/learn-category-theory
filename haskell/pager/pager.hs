b834f847dc6fdc766d1db5f74aedcca699ad1547
Wes Higbee <wes.mcclure@gmail.com>
2024-08-15 16:54:29 -0500

    notes

--- haskell/pager/pager.hs
+++ haskell/pager/pager.hs
@@ -13,7 +13,7 @@ main =
   runHCat >>= print
 
 runHCat = do
-  parseArgs <$> getArgs
+  parseArgs <$> getArgs -- <$> (aka fmap) is needed to apply func w/o IO monad wrapper, think of this as the pure functional middle part, so <$> takes this out of the IO paradigm long enough to start parsing args
 
 parseArgs :: [b] -> Either String b
 parseArgs [] = Left "you forgot to pass args"
