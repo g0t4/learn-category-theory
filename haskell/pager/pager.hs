f4bcb0416e8d6a3860c30d30b30dcc2525f61923
Wes Higbee <wes.mcclure@gmail.com>
2024-08-15 16:40:12 -0500

    simulate args

--- haskell/pager/pager.hs
+++ haskell/pager/pager.hs
@@ -8,6 +8,7 @@ noop = return ()
 myPrintArgs args = foldl (\accum current -> accum >> putStrLn current) noop args
 
 -- runhaskell pager.hs --foo bar
+-- ghci => :load pager.hs =>  System.Environment.withArgs ["--foo", "--bar"] runHCat
 main =
   runHCat
 
