de70ba2ffcd8a2fbcba2f6c4291b44c16c8ef61a
Wes Higbee <wes.mcclure@gmail.com>
2024-08-15 15:54:58 -0500

    brute force print args

--- haskell/pager.hs
+++ haskell/pager.hs
@@ -1,1 +1,13 @@
+import System.Directory.Internal.Prelude (getArgs)
+
 -- based on Effective Haskell book example, with my own improv along the way
+
+-- args = getArgs
+
+myPrintArgs [] = putStrLn ""
+myPrintArgs (head : rest) = putStrLn head >> myPrintArgs rest
+
+main = do
+  -- runhaskell pager.hs --foo bar
+  args <- getArgs
+  myPrintArgs args
