af871996355bb499d84017f02805729e05083669
Wes Higbee <wes.mcclure@gmail.com>
2024-08-16 02:06:16 -0500

    more

--- haskell/pager/mynads.hs
+++ haskell/pager/mynads.hs
@@ -119,6 +119,8 @@ testNadChains2 = do
   unwrapped5 <- (\unwrapd -> unwrapd <> "spit") `fmapMy` MyBox "bull" -- FINALLY, this is what I wanted to write way before I realized I F'd up bind on MyNads
   -- FYI can overwrite the bind var (unwrapped5) here and that makes sense as you can do that in a nested lambda too
   unwrapped5 <- (\unwrapd -> unwrapd <> "spits") <$> MyBox "bull" -- FINALLY, this is what I wanted to write way before I realized I F'd up bind on MyNads
+  unwrapped6 <- (<> "spits") <$> MyBox "bull"
+  unwrapped6 <- ("spits" <>) <$> MyBox "bull" -- more readable, esp b/c <$> looks likes $ and reads the same here
 
   -- unwrapped2
   unwrapped2 <- MyBox "fudge"
