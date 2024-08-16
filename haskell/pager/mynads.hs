ef77c4c4d3e9c7cf71f36404b1ee13bf4fce9e6c
Wes Higbee <wes.mcclure@gmail.com>
2024-08-16 02:17:10 -0500

    notes

--- haskell/pager/mynads.hs
+++ haskell/pager/mynads.hs
@@ -116,12 +116,13 @@ testNadChains2 = do
   -- unwrapped 3
   unwrapped3 <- MyBox "bull" >>= (\unwrapd -> wrap (unwrapd <> "spit"))
   unwrapped4 <- (\unwrapd -> wrap (unwrapd <> "spit")) =<< MyBox "bull" -- FINALLY, this is what I wanted to write way before I realized I F'd up bind on MyNads
-  unwrapped4b <- wrap . ("4b - spit" <>) =<< MyBox "head"
+  unwrapped4b <- wrap . ("4b - spit" <>) =<< MyBox "head" -- I dislike wrapping here, so when I wanna work with unwrapped value w/o rewrapping then use fmap/<$>
   unwrapped5 <- (\unwrapd -> unwrapd <> "spit") `fmapMy` MyBox "bull" -- FINALLY, this is what I wanted to write way before I realized I F'd up bind on MyNads
   -- FYI can overwrite the bind var (unwrapped5) here and that makes sense as you can do that in a nested lambda too
   unwrapped5 <- (\unwrapd -> unwrapd <> "spits") <$> MyBox "bull" -- FINALLY, this is what I wanted to write way before I realized I F'd up bind on MyNads
   unwrapped6 <- (<> "spits") <$> MyBox "bull"
   unwrapped6 <- ("spits" <>) <$> MyBox "bull" -- more readable, esp b/c <$> looks likes $ and reads the same here -- I REALLY LIKE THIS FORM as if you squint it looks alot like:  "spits" <> $ "bull"  => "spits" <> "bull"
+  -- TAKEAWAY => fmap/<$> is EXCELLENT to modify the unwrapped value and stay in the Monad (w/o rewrapping too), and especially to then unwrap with bind (<-) afterwards!
 
   -- unwrapped2
   unwrapped2 <- MyBox "fudge"
