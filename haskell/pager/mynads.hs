5f1e1ce296d66654ea0fd2b397706a7f962a021b
Wes Higbee <wes.mcclure@gmail.com>
2024-08-15 20:01:28 -0500

    this book, why so comlicated right away

--- haskell/pager/mynads.hs
+++ haskell/pager/mynads.hs
@@ -3,3 +3,17 @@ import GHC.Base
 testConditionalPrint = do
   when True (print "when true")
   when False (print "when false")
+
+-- Functor, (think Functee?), like Employer/Employee?
+-- Functor is a thing that can apply a function to a context? IOTW it wouldn't have the function, nor the instance... so it would take both of those as parameters (i.e. bind, fmap)
+--
+
+class MyFunctor f where
+  fmapMy :: (a -> b) -> f a -> f b
+  (<$) :: a -> f b -> f a
+  (<$) a fb = fmapMy (const a) fb
+
+  -- alias fmap to <$>
+  infixl 4 <$>
+  (<$>) :: (Functor f) => (a -> b) -> f a -> f b
+  (<$>) = fmapMy
