c778e67207356aea7e8cab54be0ecb0816550f4d
Wes Higbee <wes.mcclure@gmail.com>
2024-08-18 18:53:17 -0500

    basic works

--- haskell/HiD/fmt-package.hs
+++ haskell/HiD/fmt-package.hs
@@ -9,6 +9,11 @@ testFormatting =
       last_name = "bar"
       age = 100
    in -- fmt $ "name: " +|name|+ ", last: " +|last_name|+ ""
-      fmt $ "Hello, " +|| name ||+ "!"
-      -- fmt is using show on name? and even if "world" constant String passed it uses show before buidling it into string so its wrapped funny... wtf is going on.. this is the issue with all this type magic and compiler inference... it becomes next to impossible to know wtf is going to happen at runtime and/or when smth goes wrong b/c type inference is far too sophisticated to track all of how it will behave at runtime (why the same expression behaves different based on imports and type signatures is somewhat a problem IMO)
+      fmt ("Hello, " +|| name ||+ "!")
+
+-- fmt is using show on name? and even if "world" constant String passed it uses show before buidling it into string so its wrapped funny... wtf is going on.. this is the issue with all this type magic and compiler inference... it becomes next to impossible to know wtf is going to happen at runtime and/or when smth goes wrong b/c type inference is far too sophisticated to track all of how it will behave at runtime (why the same expression behaves different based on imports and type signatures is somewhat a problem IMO)
 -- cabal repl -- b/c this deps on Fmt
+
+testBasic =
+  let name = "Alice" :: String
+   in "Meet " +| name |+ "!" :: String
