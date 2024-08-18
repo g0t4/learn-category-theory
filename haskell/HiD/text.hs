d856ad770f34014bb3ee37736801fe06342d0651
Wes Higbee <wes.mcclure@gmail.com>
2024-08-18 17:36:41 -0500

    droparound and fix test mytakewhile

--- haskell/HiD/text.hs
+++ haskell/HiD/text.hs
@@ -4,8 +4,18 @@ import Data.Char (isLetter)
 myTakeWhile :: (a -> Bool) -> [a] -> [a]
 myTakeWhile _ [] = []
 myTakeWhile func (head : rest)
-  | func (head) = head : myTakeWhile func rest
+  | func head = head : myTakeWhile func rest
   | otherwise = []
 
 testTakeWhile = do
-  print $ takeWhile isLetter "foo-the-bar"
+  print $ myTakeWhile isLetter "foo-the-bar"
+
+myDropAround :: (a -> Bool) -> [a] -> [a]
+myDropAround _ [] = []
+myDropAround func (head : rest)
+  | func head = myDropAround func rest -- drop if match predicate? IncoherentInstances
+  | otherwise = head : myDropAround func rest
+
+testDropAround = do
+  print $ myDropAround isLetter "foo-the-bar"
+  print $ myDropAround (not . isLetter) "foo-the-bar"
